7aadcd17ff6336a9c954a9c3c5d7cd4eb2b21d9d
Guillaume Grenet <ggrenet@gmail.com>
2018-08-14 13:29:23 +0200

    Update endpoint documentation for KintoHub

--- server.rb
+++ server.rb
@@ -1,41 +1,31 @@
 require 'sinatra'
 require 'rest-client'
 require 'json'
-
-get '/' do
+=begin
+@api {GET} /{ip} Request localisation of an IP
+@apiName WhereAmI
+@apiParam {String} ip IPV4 IP.
+@apiSuccess {String} zipcode Zipcode
+@apiSuccess {String} city Name of the city
+@apiSuccess {String} country Name of the country
+@apiError (500) {String} error Critical server error
+@apiError (400) {String} error Error from ipstack
+=end
+get '/:ip' do
   begin
-    response = JSON.parse(
-      RestClient.get("http://api.ipstack.com/#{request.ip}?access_key=c71f08afcbabf32bc9066cc6be22d157")
-    )
-    [200, <<~EOS
-      <html>
-        <head>
-          <title>Where Am I?</title>
-        </head>
-        <body>
-          <h1>There you go!</h1>
-          <p>Your ip: #{request.ip}</p>
-          <p>
-            <img src="#{response.dig("location", "country_flag")}">
-            <span>#{response["city"]}, #{response["zip"]}, #{response["country_name"]}</span>
-          </p>
-        </body>
-      </html>
-    EOS
-    ]
+    response = RestClient.get("http://api.ipstack.com/#{params['ip']}?access_key=c71f08afcbabf32bc9066cc6be22d157")
+    return [400, { error: response.body, status: response.code }.to_json] unless response.code == 200
+    data = JSON.parse(response.body)
+    if data.dig("zip").nil? && data.dig("city").nil? && data.dig("country_name").nil?
+      [400, { error: "Couldn't find any localisation data for #{params['ip']}" }.to_json]
+    else
+      [200, {
+        zipcode: data.dig("zip"),
+        city: data.dig("city"),
+        country: data.dig("country_name"),
+      }.to_json]
+    end
   rescue StandardError => exception
-    [500, <<~EOS
-      <html>
-        <head>
-          <title>Where Am I?</title>
-        </head>
-        <body>
-          <h1>An error occured.</h1>
-          <p>Woops. Try again later!</p>
-          <p>Error: #{exception.to_s}</p>
-        </body>
-      </html>
-    EOS
-    ]
+    [500, { error: exception.to_s }.to_json]
   end
 end
